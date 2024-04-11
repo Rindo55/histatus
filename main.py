31c3c09810eeb6332aca5dc8f62bf0c0ead94aef
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-11 10:56:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot_token = "6428443845:AAF9usGZRMRPPMuOfcjClNypt3N_p2_gUZc"
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Website URL to check
-url = "https://ddl animxt.fun"
+url = "https://hianime.to"
 website_down = False
 
 # Function to check the website status
@@ -20,15 +20,15 @@ def check_website_status():
         response = requests.get(url)
         if response.status_code != 200:
             if not website_down:
-                app.send_message(-1002029401168, f"HiAnime is Reporting Error\nHTTP ERROR {response.status_code}")
+                asyncio.run(app.send_message(-1002029401168, f"HiAnime is Reporting Error\nHTTP ERROR {response.status_code}"))
                 website_down = True
         else:
             if website_down:
-                app.send_message(-1002029401168, "HiAnime is Available\nAvailable")
+                asyncio.run(app.send_message(-1002029401168, "HiAnime is Available\nAvailable"))
                 website_down = False
     except requests.ConnectionError:
         if not website_down:
-            app.send_message(-1002029401168, "HiAnime is Reporting Error\nConnection Error")
+            asyncio.run(app.send_message(-1002029401168, "HiAnime is Reporting Error\nConnection Error"))
             website_down = True
 
 # Background task to check website status every minute
