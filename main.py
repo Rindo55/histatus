fd3d0c521ef7e5082ca0b871b9ed46ab4ed08aa2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-11 10:10:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ api_hash = "95937bcf6bc0938f263fc7ad96959c6d"
 bot_token = "6428443845:AAF9usGZRMRPPMuOfcjClNypt3N_p2_gUZc"
 
 app = Client("website_status_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-app.start()
-print("Powered by hianime.to")
-idle()
+
 # Function to check the website status
 def check_website_status():
     url = "https://hianime.to"
@@ -24,22 +22,22 @@ def check_website_status():
 website_down = False
 
 # Function to send messages to the channel
-def send_message(message):
-    app.send_message(-1002029401168, message)
 
 # Main loop to check website status periodically
 while True:
     website_is_down, error_code = check_website_status()
     
     if website_is_down and not website_down:
-        send_message(f"**HiAnime is Reporting Error**\n\n`HTTP ERROR {error_code}`")
+        app.start()
+        app.send_message(-1002029401168, f"**HiAnime is Reporting Error**\n\n`HTTP ERROR {error_code}`")
         website_down = True
     elif not website_is_down and website_down:
-        send_message("**HiAnime is Available**\n\n`Available`")
+        app.start()
+        app.send_message(-1002029401168, "**HiAnime is Available**\n\n`Available`")
         website_down = False
     
     # Check every 5 minutes
     time.sleep(120)
 
 # Start the bot
-
+app.run
